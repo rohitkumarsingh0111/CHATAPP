657e725291440587f8af3f2a8dfd77fe6e763d77
rohitsinghabad <rohitsinghrajputabad.com>
2025-11-28 23:32:06 +0530

    Updated auth controller

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req, res) => {
 
     try{
         if(!fullName || !email || !password){
-            return res.status(400).json({message: "All fields are required" });
+            return res.status(400).json({message: "All fields are required"});
         }
         
         if(password.length < 6) {
