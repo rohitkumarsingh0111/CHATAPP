0af91e9df9a1656de4a4488404d2f9efa95ed006
rohitsinghabad <rohitsinghrajputabad.com>
2025-12-02 19:23:46 +0530

    message route optimized

--- backend/src/models/message.js
+++ backend/src/models/message.js
@@ -14,6 +14,9 @@ const messageSchema = new mongoose.Schema(
         },
         text: {
             type: String,
+            trim: true,
+            maxlength: 2000,
+
 
         },
         image: {
