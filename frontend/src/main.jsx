15bb8998cc911aa714870609e89cbe288ba9396a
rohitsinghabad <rohitsinghrajputabad@gmail.com>
2025-12-15 04:29:41 +0530

    PWA implemented

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  immediate: true,
+})
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        id: '/',
+        name: 'ChatApp',
+        short_name: 'ChatApp',
+        description: 'Real-time chat application',
+        theme_color: '#0f172a',
+        background_color: '#0f172a',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+  {
+    src: '/pwa-192x192.png',
+    sizes: '192x192',
+    type: 'image/png',
+    purpose: 'any'
+  },
+  {
+    src: '/pwa-512x512.png',
+    sizes: '512x512',
+    type: 'image/png',
+    purpose: 'any'
+  }
+]
+
+      }
+    })
+  ]
 })
