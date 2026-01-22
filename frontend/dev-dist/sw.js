87b4411356d083d856a1d30d8bef7dfa970b7aa6
rohitsinghabad <rohitsinghrajputabad@gmail.com>
2026-01-22 22:49:25 +0530

    Update baseline-browser-mapping dependency to version 2.9.17 and revise precache revision in service worker

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.llhkr37hhsg"
+    "revision": "0.tvuklr5s85"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
